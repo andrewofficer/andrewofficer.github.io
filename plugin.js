c7ef082f914bfe8c582e9999a6a782a4ff27188c
andrewofficer <andrewofficer@gmail.com>
2016-08-24 21:32:55 +0100

    Moved script to plugin.js, copy changes and added GA tracking

--- plugin.js
+++ plugin.js
@@ -565,3 +565,40 @@
 
 })(jQuery);
 /*! End of homepage and product carousel */
+
+/*! Start in-page JS */
+$(document).ready(function() {
+	$("#carousel-1-0-tab0").focus();
+	console.log("LOLLOLOL");
+	$('.slidewrap').carousel({
+		slider: '.slider',
+		slide: '.slide',
+		slideHed: '.slidehed',
+		nextSlide : '.next',
+		prevSlide : '.prev',
+		addPagination: true,
+		addNav : false
+	});
+
+	$('.slidewrap2').carousel({
+		slider: '.slider',
+		slide: '.slide',
+		addNav: false,
+		addPagination: true,
+		speed: 300 // ms.
+	});
+
+	$('.slidewrap3').carousel({
+			namespace: "mr-rotato" // Defaults to “carousel”.
+		})
+		.bind({
+			'mr-rotato-beforemove' : function() {
+				$('.events').append("<li>“beforemove” event fired.</li>");
+			},
+			'mr-rotato-aftermove' : function() {
+				$('.events').append("<li>“aftermove” event fired.</li>");
+			}
+		})
+		.after('<ul class="events">Events</ul>');
+});
+/*! End in-page JS */
